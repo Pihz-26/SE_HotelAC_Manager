4b4ce648d1c3eaf30aaccc40c7b767ec5465f85d
PHZ20040126 <2892270442@qq.com>
2024-11-27 20:55:47 +0800

    update

--- front/request_body.py
+++ front/request_body.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
+from typing import Literal
 
 
 class FanRate(BaseModel):
@@ -10,9 +11,9 @@ class FanRate(BaseModel):
 # 房间空调状态设置的请求体
 class RoomACStatusControlRequest(BaseModel):
     roomId: int                # 房间号，类型为整型
-    power: str                 # 电源状态，例如 "on" 或 "off"
+    power: Literal["on", "off"]               # 电源状态，例如 "on" 或 "off"
     temperature: int           # 温度，类型为整型
-    windSpeed: str             # 风速，可能为 "低", "中", "高" 等
+    windSpeed: Literal["低", "中", "高"]             # 风速，可能为 "低", "中", "高" 等
     sweep: str    
     
 class AdminLoginRequest(BaseModel):
